d9d3a27554908f56ea7523462118c604fc982aa8
Caroline Chan <carolinechan304@gmail.com>
2022-09-20 17:17:12 +0700

    Displaying data in a list

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -1,29 +1,21 @@
-//
-//  CardView.swift
-//  Scrumdinger
-//
-//  Created by Caroline Chan on 17/09/22.
-//
-
 import SwiftUI
 
 struct CardView: View {
     let scrum: DailyScrum
+
     var body: some View {
         VStack(alignment: .leading) {
-            Text("\(scrum.title)")
-                    .font(.headline)
-                    .accessibilityAddTraits(.isHeader)
-                
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
-                
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
                     .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
-                Label("\(scrum.lengthInMinutes) minutes", systemImage: "clock")
-                    .labelStyle(.trailingIcon)
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+                    .labelStyle(.trailingIcon)
             }
             .font(.caption)
         }
@@ -33,7 +25,7 @@ struct CardView: View {
 }
 
 struct CardView_Previews: PreviewProvider {
-    static var scrum: DailyScrum = DailyScrum.sampleData[0]
+    static var scrum = DailyScrum.sampleData[0]
     static var previews: some View {
         CardView(scrum: scrum)
             .background(scrum.theme.mainColor)
