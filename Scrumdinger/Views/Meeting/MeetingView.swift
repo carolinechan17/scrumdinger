2884935c51e00c5e71654c54333adb6f4c5595b0
Caroline Chan <carolinechan304@gmail.com>
2022-10-04 14:06:26 +0700

    Add a state object for the source of truth

--- Scrumdinger/Views/Meeting/MeetingView.swift
+++ Scrumdinger/Views/Meeting/MeetingView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16)
                 .fill(scrum.theme.mainColor)
             VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 ProgressView(value: 20, total: 100)
                 
                 HStack {
